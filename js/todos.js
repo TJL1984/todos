b92ebe67ecc5bddb809f448c2f8ec245f186d197
TJ Liwanag <timothyjliwanag@gmail.com>
2016-10-28 15:26:00 -0400

    began by applying new variable for the window

---
+++ js/todos.js
@@ -0,0 +1,15 @@
+
+(function() {
+  window.todo = window.todo;
+} // new variable on the window thats defining itself.
+
+function addToDo(text) {
+  var article = document.createElement('article');
+
+  var
+
+
+
+
+
+}() );
